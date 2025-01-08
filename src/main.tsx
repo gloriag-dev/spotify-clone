569e422a10ff8d84c458336927518c85c0ccfb30
Gloria <giannascoli.gloria@gmail.com>
2025-01-08 18:11:42 +0100

    create .env file to store variables

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,20 @@ import "./index.css";
 import App from "./App.tsx";
 import MSWgateway from "./custom/MSWgateway/MSWgateway.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Auth0Wrapper } from "./auth/Auth0Wrapper/Auth0Wrapper.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <MSWgateway>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </MSWgateway>
+    <BrowserRouter>
+      <Auth0Wrapper>
+        <MSWgateway>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </MSWgateway>
+      </Auth0Wrapper>
+    </BrowserRouter>
   </StrictMode>
 );
