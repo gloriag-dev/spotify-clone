09f271ed3f23b4986906245f6db67ed6d5e2e3de
Gloria <giannascoli.gloria@gmail.com>
2025-01-09 19:31:21 +0100

    fix

--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -1,4 +1,4 @@
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { AuthLoginCallback } from "../../../../Desktop/online-menu/src/auth/AuthLoginCallback/AuthLoginCallBack";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 import { PublicRoutes } from "./PublicRoutes";
@@ -6,9 +6,13 @@ import { PublicRoutes } from "./PublicRoutes";
 const Routing = () => {
   return (
     <div>
-      <Route path="/auth/login/callback" element={<AuthLoginCallback />} />
-      <Route path="/app/*" element={<ProtectedRoutes />} />
-      <Route path="*" element={<PublicRoutes />} />
+      <Router>
+        <Routes>
+          <Route path="/auth/login/callback" element={<AuthLoginCallback />} />
+          <Route path="/app/*" element={<ProtectedRoutes />} />
+          <Route path="*" element={<PublicRoutes />} />
+        </Routes>
+      </Router>
     </div>
   );
 };
